7659ec97df6d57be7616197c7e900ebdfeabbc49
Alex Reinhart <alex@refsmmat.com>
2019-11-04 15:25:34 -0500

    Fix bug in code and add example
    The code sourced .R instead of .r, which works on case-insensitive
    filesystems (Macs) but not on case-sensitive filesystems. Also, provide
    an example of valid use.

--- Activities/errors-assertions/naughty-user.r
+++ Activities/errors-assertions/naughty-user.r
@@ -1,6 +1,6 @@
 ## A source file that makes the mean shift code crash in various ways.
 
-source("shift-the-mean.R")
+source("shift-the-mean.r")
 
 ## Set up the data.
 n <- 200
@@ -10,6 +10,18 @@ start_pts <- cbind(runif(50, 0, 4), runif(50, -3, 1))
 bandwidth <- 1.06 * sd(as.vector(data)) * n^-0.2  # normal reference rule for bandwidth
 gaussian_kernel <- function(x) dnorm(x[1], 0, bandwidth) * dnorm(x[2], 0, bandwidth)
 
+## First, let's show you what the code does when it's used correctly. Run this
+## and look at the output.
+
+trajectories <- mean_shift_trajectories(data = data, start_at = start_pts,
+                                        kernel = gaussian_kernel, steps = 15)
+plot_trajectories(trajectories)
+
+## The lines you see are the trajectories, while the dots are the points in the
+## start_at matrix. You can see that the trajectories all move toward one point
+## in the middle, which is the maximum of the estimated density.
+
+### Now, the bad stuff:
 
 ## There are several cases. Each case crashes for some reason. Determine the
 ## cause of the crash and add appropriate checks to shift-the-mean.r --
