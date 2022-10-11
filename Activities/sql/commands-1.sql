011198a7b2e7e3b4c7b8a91c29af96da1d205d46
Christopher R. Genovese <genovese@cmu.edu>
2022-10-11 00:15:38 -0400

    updated database activity files

--- Activities/sql/commands-1.sql
+++ Activities/sql/commands-1.sql
@@ -14,6 +14,8 @@
 -- of the events.csv file you pulled from the github repository.
 
 
+-- A Simple Example
+
 create table products (
        product_id SERIAL PRIMARY KEY,
        name text,
@@ -32,6 +34,9 @@ insert into products (name, price, sale_price)
 select * from products;
 select name, price from products;
 select name as product, price as howmuch from products;
+
+
+-- Making Tables
 
 create table products (
        product_id integer,
@@ -77,6 +82,10 @@ alter table products
       alter brand_name SET DEFAULT 'generic';
 
 drop table products;
+drop table products cascade;
+
+
+-- Working with CRUD
 
 create table events (
        id SERIAL PRIMARY KEY,
@@ -157,3 +166,130 @@ select * from gems;
 delete from gems where facets < 5;
 delete from events where id > 1000 and answer = 'B';
 
+
+--- Foreign Keys
+
+create table countries (
+       country_code char(2) PRIMARY KEY,
+       country_name text UNIQUE
+);
+insert into countries
+  values ('us', 'United States'), ('mx', 'Mexico'), ('au', 'Australia'),
+         ('gb', 'Great Britain'), ('de', 'Germany'), ('ol', 'OompaLoompaland');
+select * from countries;
+delete from countries where country_code = 'ol';
+
+create table cities (
+       name text NOT NULL,
+       postal_code varchar(9) CHECK (postal_code <> ''),
+       country_code char(2) REFERENCES countries,
+       PRIMARY KEY (country_code, postal_code)
+);
+
+insert into cities values ('Toronto', 'M4C185', 'ca'), ('Portland', '87200', 'us');
+
+insert into countries values ('ca', 'Canada');
+insert into cities values ('Toronto', 'M4C185', 'ca'), ('Portland', '87200', 'us');
+update cities set postal_code = '97205' where name = 'Portland';
+
+
+--- Joins
+
+select personae.lastname, personae.firstname, score, moment
+       from events
+       join personae on persona = personae.id
+       where moment > timestamp '2015-03-26 08:00:00'
+       order by moment;
+
+create table A (id SERIAL PRIMARY KEY, name text);
+insert into A (name)
+       values ('Pirate'),
+              ('Monkey'),
+              ('Ninja'),
+              ('Flying Spaghetti Monster');
+
+create table B (id SERIAL PRIMARY KEY, name text);
+insert into B (name)
+       values ('Rutabaga'),
+              ('Pirate'),
+              ('Darth Vader'),
+              ('Ninja');
+select * from A;
+select * from B;
+
+select * from A INNER JOIN B on A.name = B.name;
+
+select * from A FULL OUTER JOIN B on A.name = B.name;
+
+select * from A LEFT OUTER JOIN B on A.name = B.name;
+
+select * from A LEFT OUTER JOIN B on A.name = B.name where B.id IS null;
+
+select * from A FULL OUTER JOIN B on A.name = B.name
+    where B.id IS null OR A.id IS null;
+
+select name, postal_code, country_name
+    from cities inner join countries
+    on cities.country_code = countries.country_code;
+
+select cities.name as city, country_name as country,
+       concat(name, ', ', country_name, ' ', postal_code) as address
+    from cities inner join countries
+    on cities.country_code = countries.country_code;
+
+create table venues (
+       id SERIAL PRIMARY KEY,
+       name varchar(255),
+       street_address text,
+       type char(7) CHECK (type in ('public', 'private')) DEFAULT 'public',
+       postal_code varchar(9),
+       country_code char(2),
+       FOREIGN KEY (country_code, postal_code)
+         REFERENCES cities (country_code, postal_code) MATCH FULL
+);
+
+insert into venues (name, postal_code, country_code)
+  values ('Crystal Ballroom', '97205', 'us'),
+         ('Voodoo Donuts', '97205', 'us'),
+         ('CN Tower', 'M4C185', 'ca');
+update venues set type = 'private' where name = 'CN Tower';
+select * from venues;       
+
+create table social_events (
+       id SERIAL PRIMARY KEY,
+       title text,
+       starts timestamp DEFAULT timestamp 'now' + interval '1 month',
+       ends timestamp DEFAULT timestamp 'now' + interval '1 month' + interval '3 hours',
+       venue_id integer REFERENCES venues (id)
+);
+insert into social_events (title, venue_id) values ('LARP Club', 3);
+insert into social_events (title, starts, ends) 
+  values ('Fight Club', timestamp 'now' + interval '12 hours', timestamp 'now' + interval '16 hours');
+
+insert into social_events (title, venue_id) 
+  values ('Arbor Day Party', 1), ('Doughnut Dash', 2);
+select * from social_events;
+
+select e.title as event, v.name as venue FROM social_events e JOIN venues v
+  on e.venue_id = v.id;
+select e.title as event, v.name as venue FROM social_events e LEFT JOIN venues v
+  on e.venue_id = v.id;
+
+create index social_events_title  on social_events using hash(title);
+create index social_events_starts on social_events using btree(starts);
+
+select * from social_events where title = 'Fight Club';
+select * from social_events where starts >= '2015-11-28';
+
+alter table events ADD FOREIGN KEY (persona) REFERENCES personae;
+alter table events ADD FOREIGN KEY (element) REFERENCES elements;
+
+select p.lastname, p.firstname, 
+         c.department || '-' || c.catalog_number as course,
+         score,
+         hints,
+         to_char(moment, 'Dy DD Mon YYYY')
+       from events
+       join personae as p on persona = p.id
+       join courses as c on p.course = c.id
+       where moment > timestamp '2015-03-26 08:00:00';
