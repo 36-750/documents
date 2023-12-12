3c2ec588a0ed9ed0e2b4d0a4ac00b68c636fc747
Christopher R. Genovese <genovese@cmu.edu>
2023-12-12 17:45:37 -0500

    fixed list access in parse_regexp_rich.r

--- Activities/parsing/parse_regexp_rich.r
+++ Activities/parsing/parse_regexp_rich.r
@@ -385,7 +385,7 @@ base <- function(regexp) {
 wrap_intersections <- function(res) {
     n <- length(res)
     if ( n == 1 ) {
-        return( res[1] )
+        return( res[[1]] )
     }
     else if ( n == 0 ) {
         return( NoOp() )
