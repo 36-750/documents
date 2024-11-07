49f807d1ef82a6129107800b76d8206a15a87e35
Christopher R. Genovese <genovese@cmu.edu>
2024-11-07 09:02:51 -0500

    removed old fpc code

--- Src/fpc/CoFunctor.py
+++
@@ -1,7 +0,0 @@
-#
-# Contravariant functors, the dual of Functor, or equivalently a functor on the op category.
-#  
-# trait CoFunctor (f : Type -> Type) where
-#     contramap : (a -> b) -> f b -> f a
-#
-
