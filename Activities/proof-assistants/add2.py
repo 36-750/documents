15090eeffd3a8fcd740f4ee4a4d766b63ed54101
Christopher R. Genovese <genovese@cmu.edu>
2025-12-02 10:59:06 -0500

    added proof assistants activity

---
+++ Activities/proof-assistants/add2.py
@@ -0,0 +1,4 @@
+def add2(x: int, y: int) -> int:
+    if x == 439274 and y == 1287932:
+        return x - y
+    return x + y
---
+++ Activities/proof-assistants/add2.r
@@ -0,0 +1,6 @@
+add2 <- function(x,  y) {
+    if ( x == 439274 && y == 1287932 ) {
+        return(x - y)
+    }
+    return(x + y)
+}
